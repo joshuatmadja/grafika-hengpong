07cb262073112d20a19df1420ba9cc0807d3b4b9
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2017-04-25 16:29:44 +0700

    Add mesh for phone

--- main.cpp
+++ main.cpp
@@ -10,20 +10,7 @@ using namespace std;
 using namespace glm;
 
 int main() {
-    Display display(800, 600, "Handphone");
-/*    Vertex head_vertices[] =    {
-                                    Vertex(vec3(0, 0, 0)),
-                                    Vertex(vec3(-0.25, 0.0, 0)),
-                                    Vertex(vec3(-0.25, 0.25, 0)),
-                                    Vertex(vec3(0.0, 0.25, 0)),
-                                }; */
-
-
-    /* Vertex eye_vertices[] =     {
-                                    Vertex(vec3(-0.2, 0.15, 0)),
-                                    Vertex(vec3(-0.15, 0.2, 0)),
-                                    Vertex(vec3(-0.17, 0.15, 0)),
-                                }; */
+    Display display(750, 750, "Oppo Neo 7");
 
     Vertex body_vertices[] =    {
                                     Vertex(vec3(-0.5, -0.5, 0), glm::vec2(0, 0)),
@@ -31,80 +18,95 @@ int main() {
                                     Vertex(vec3(0.5, -0.5, 0), glm::vec2(1, 0))
                                 };
 
-/*    Vertex mouth_vertices[] =   {
-                                    Vertex(vec3(-0.25, 0, 0)),
-                                    Vertex(vec3(-0.25, 0.13, 0)),
-                                    Vertex(vec3(-0.15, 0.05, 0)),
-                                };
-
-    Vertex wing_vertices[] =    {
-                                    Vertex(vec3(0, 0, 0)),
-                                    Vertex(vec3(0.5, 0.5, 0)),
-                                    Vertex(vec3(0.375, 0.25, 0)),
-                                    Vertex(vec3(0.5, 0, 0)),
-                                    Vertex(vec3(0.375, -0.25, 0)),
-                                    Vertex(vec3(0.5, -0.5, 0)),
-                                    Vertex(vec3(0.055, -0.4, 0)),
-                                };
-
-    Vertex hands_vertices[] =   {
-                                    Vertex(vec3(0.055, -0.4, 0)),
-                                    Vertex(vec3(-0.055, -0.7, 0)),
-                                    Vertex(vec3(-0.055, -0.1, 0)),
-                                };
-
-    Vertex foots_vertices[] =   {
-                                    Vertex(vec3(0, -0.125, 0)),
-                                    Vertex(vec3(-0.15, -0.13, 0)),
-                                    Vertex(vec3(0, -0.2, 0)),
-
-                                    Vertex(vec3(0, -0.225, 0)),
-                                    Vertex(vec3(-0.25, -0.23, 0)),
-                                    Vertex(vec3(0, -0.2, 0)),
-                                };
-    Vertex horns_vertices[] =   {
-                                    Vertex(vec3(-0.25, 0.25, 0)),
-                                    Vertex(vec3(-0.125, 0.375, 0)),
-                                    Vertex(vec3(-0.2, 0.25, 0)),
-
-                                    Vertex(vec3(-0.075, 0.375, 0)),
-                                    Vertex(vec3(-0.15, 0.25, 0)),
-                                }; */
+    float scale = 3;
     Vertex phone_front[] = {
-                            Vertex(vec3(-0.25, 0.75, 0), glm::vec2(0, 0)),
-                            Vertex(vec3(0.25, 0.75, 0), glm::vec2(0, 0)),
-                            Vertex(vec3(0.25, -0.75, 0), glm::vec2(0, 0)),
-                            Vertex(vec3(-0.25, -0.75, 0), glm::vec2(0, 0))
+                            Vertex(vec3(-0.141*scale, 0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, 0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, -0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(-0.141*scale, -0.281*scale, 0), glm::vec2(0, 0))
+                            };
+    Vertex phone_back[] = {
+                            Vertex(vec3(-0.141*scale, 0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, 0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, -0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(-0.141*scale, -0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            };
+    Vertex phone_left[] = {
+                            Vertex(vec3(-0.141*scale, 0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(-0.141*scale, 0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(-0.141*scale, -0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(-0.141*scale, -0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            };
+    Vertex phone_right[] = {
+                            Vertex(vec3(0.141*scale, 0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, 0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, -0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, -0.281*scale, 0), glm::vec2(0, 0)),
+                            };
+    Vertex phone_top[] = {
+                            Vertex(vec3(-0.141*scale, 0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, 0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, 0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(-0.141*scale, 0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            };
+    Vertex phone_bottom[] = {
+                            Vertex(vec3(-0.141*scale, -0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, -0.281*scale, 0), glm::vec2(0, 0)),
+                            Vertex(vec3(0.141*scale, -0.281*scale, -0.015*scale), glm::vec2(0, 0)),
+                            Vertex(vec3(-0.141*scale, -0.281*scale, -0.015*scale), glm::vec2(0, 0)),
                             };
 
-//    Mesh head(head_vertices, sizeof(head_vertices)/sizeof(head_vertices[0]));
-//    Mesh eye(eye_vertices, sizeof(eye_vertices)/sizeof(eye_vertices[0]));
     Mesh body(body_vertices, sizeof(body_vertices)/sizeof(body_vertices[0]));
-//    Mesh mouth(mouth_vertices, sizeof(mouth_vertices)/sizeof(mouth_vertices[0]));
-//    Mesh wing(wing_vertices, sizeof(wing_vertices)/sizeof(wing_vertices[0]));
-//    Mesh hands(hands_vertices, sizeof(hands_vertices)/sizeof(hands_vertices[0]));
-//    Mesh foots(foots_vertices, sizeof(foots_vertices)/sizeof(foots_vertices[0]));
-//    Mesh horns(horns_vertices, sizeof(horns_vertices)/sizeof(horns_vertices[0]));
 
-    Shader red_shader("./res/redShader");
-    Shader green_shader("./res/greenShader");
-    Shader white_shader("./res/whiteShader");
-    Shader black_shader("./res/blackShader");
-    Shader blue_shader("./res/blueShader");
+    Mesh front_phone(phone_front, sizeof(phone_front)/sizeof(phone_front[0]));
+    Mesh back_phone(phone_back, sizeof(phone_back)/sizeof(phone_back[0]));
+    Mesh left_phone(phone_left, sizeof(phone_left)/sizeof(phone_left[0]));
+    Mesh right_phone(phone_right, sizeof(phone_right)/sizeof(phone_right[0]));
+    Mesh top_phone(phone_top, sizeof(phone_top)/sizeof(phone_top[0]));
+    Mesh bottom_phone(phone_bottom, sizeof(phone_bottom)/sizeof(phone_bottom[0]));
+
+
+//    Shader red_shader("./res/redShader");
+//    Shader green_shader("./res/greenShader");
+//    Shader white_shader("./res/whiteShader");
+//    Shader black_shader("./res/blackShader");
+//    Shader blue_shader("./res/blueShader");
     Shader shader("./res/textureShader");
-    Texture texture("./res/bricks.jpg");
+//    Texture texture("./res/bricks.jpg");
+    Texture frontTexture("./res/Phone-front.png");
+    Texture backTexture("./res/Phone-back.png");
+    Texture leftTexture("./res/Phone-left.png");
+    Texture rightTexture("./res/Phone-right.png");
+    Texture topTexture("./res/Phone-top.png");
+    Texture bottomTexture("./res/Phone-bottom.png");
+
     Transform transform;
     float counter = 0.0f;
 
     while (!display.IsClosed()) {
         display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
  //       transform.GetPos().x = sinf(counter);
-//        transform.GetRot().z = counter;
+        transform.GetRot().z = counter;
         shader.Bind();
-        texture.Bind(0);
         shader.Update(transform);
-//        head.DrawPolygon();
-        body.DrawPolygon();
+
+        frontTexture.Bind(0);
+        front_phone.DrawPolygon();
+
+        backTexture.Bind(0);
+        back_phone.DrawPolygon();
+
+        leftTexture.Bind(0);
+        left_phone.DrawPolygon();
+
+        rightTexture.Bind(0);
+        right_phone.DrawPolygon();
+
+        topTexture.Bind(0);
+        top_phone.DrawPolygon();
+
+        bottomTexture.Bind(0);
+        bottom_phone.DrawPolygon();
         // hands.DrawPolygon();
         // foots.DrawPolygon();
 
